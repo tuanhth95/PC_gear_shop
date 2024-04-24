fd72bad9fade9a7cfdbb90497627963ac670b083
Camtu9 <146794610+Camtu9@users.noreply.github.com>
2024-04-24 19:39:18 +0700

    commit search

---
+++ BACKEND_SEARCH/src/models/ProductModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+    {
+        id: {type: Number, required: true},
+        name: {type: String, required: true, unique: true},
+        img: {type: String, required: true},
+        type: {type: String, required: true},
+        price: {type: Number, required: true},
+        countInStock: {type: Number, required: true},
+        discount: {type: Number, required: true},
+        description: {type: String},
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
