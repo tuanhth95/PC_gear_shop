05c70b5cb4e8e38b86e82e7ffce99481a70bd20c
CamVan16 <21521665@gm.uit.edu.vn>
2024-05-03 21:18:20 +0700

    admin_media_carousel

--- pc-gear-server/src/routes/index.js
+++ pc-gear-server/src/routes/index.js
@@ -5,6 +5,9 @@ const ReviewRouter = require('./ReviewRouter')
 const categoriesRouter = require('./categoryRoute')
 const ProductDetailRouter = require('./ProductDetailRouter')
 const OrderDetailRouter = require('./OrderDetailRouter')
+const sendEmail = require('./emailRouter')
+const ImgProduct = require('./ImgRouter')
+const Carousel = require('./CarouselRouter')
 const routes = (app) => {
   app.use('/cart', CartRouter)
   app.use('/api/user', userRouter)
@@ -13,5 +16,8 @@ const routes = (app) => {
   app.use("/api/categories", categoriesRouter);
   app.use('/api/product_detail', ProductDetailRouter);
   app.use('/api/OrderDetail', OrderDetailRouter);
+  app.use('/api/user', sendEmail)
+  app.use('/api/img', ImgProduct)
+  app.use('/api/carousel', Carousel)
 }
 module.exports = routes
